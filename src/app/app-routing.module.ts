e5c959037bd599796ef03206fe47f9f834435d2d
leonardoaf2 <leeo.affonso@hotmail.com>
2021-08-26 18:06:45 -0300

    Adicionando rotas e implementação do componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , redirectTo: 'landing-page', pathMatch: 'full'},
+
+  {path: 'cadastrar' , component:CadastrarComponent},
+  {path: 'landing-page' , component:LandingPageComponent},
+  {path: 'sobre' , component:SobreComponent},
+  {path: 'login' , component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
