520bd75c52e1c6ebd7749f5986b17588b3a875ca
bbaldaia <brunobfreire98@gmail.com>
2021-09-01 11:06:37 -0300

    Criação da model

---
+++ src/app/model/Produto.ts
@@ -0,0 +1,14 @@
+import { Categoria } from "./Categoria";
+import { Usuario } from "./Usuario";
+
+export class Produto {
+    
+    public id: number;
+    public nome: string;
+    public quantidade: number;
+    public garantia: string;
+    public descricao: string;
+    public valor: number;
+    public categoria: Categoria;
+    public usuario: Usuario;
+}
---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin {
+
+    public id: number;
+    public email: string;
+    public nome: string;
+    public senha: string;
+    public token: string;
+    public tipo: string;
+    public foto: string;
+}
